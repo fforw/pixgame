3d21afd20968a678338978a6addc00dfa91b8576
Sven Helmberger <fforw@gmx.de>
2019-07-22 16:45:01 +0200

    Tooling: Clean up workers, comment out png for now

--- tooling/clean.js
+++ tooling/clean.js
@@ -3,6 +3,7 @@ const shelljs = require("shelljs");
 
 shelljs.rm(
     path.join(__dirname, "../docs/bundle-*"),
-    path.join(__dirname, "../docs/*.png"),
+    path.join(__dirname, "../docs/*.worker.js*"),
+//    path.join(__dirname, "../docs/*.png"),
 
 );
